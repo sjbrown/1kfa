f07fb9356ff99dee97b3df5e749b722ac2229a66
Shandy Brown <github@ezide.com>
2026-05-06 11:15:26 -0700

    build_all.py to python3

--- bin/build_all.py
+++ bin/build_all.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/env python3
 
 import os, sys
 
@@ -6,17 +6,16 @@ DEBUG = os.environ.get('DEBUG', 1)
 
 def run(cmd):
     if DEBUG:
-        print cmd
+        print(cmd)
     status = os.system(cmd)
     if status != 0:
-        print 'Error', status
+        print('Error', status)
         sys.exit(status)
 
 run('pandoc --version')
 run('bash bin/build_pdf_guides.sh')
-run('cd resolution_cards; python process.py')
-run('cd resolution_cards; python process_tall.py')
-run('cd resolution_cards; python process_tenstep.py')
-run('cd resolution_cards; python process_print_and_play.py')
+run('cd resolution_cards; python3 process.py')
+run('cd resolution_cards; python3 process_tall.py')
+run('cd resolution_cards; python3 process_tenstep.py')
+run('cd resolution_cards; python3 process_print_and_play.py')
 run('bash bin/gzip_artifacts.sh')
-
